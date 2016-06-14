5cd53b8bf06394b35b1e63063c045a8ae413734c
TiagoAssun <tiagoassun@outlook.com>
2016-06-14 18:01:49 -0300

    Adicionado telas de todos os menus

--- src/tiagoassun/ifes/bsi/poo1/TelaEvento.java
+++ src/tiagoassun/ifes/bsi/poo1/TelaEvento.java
@@ -4,46 +4,175 @@ import javax.swing.*;
 
 
 public class TelaEvento {
-    private StringBuilder menu;
+    private StringBuilder menu_agenda;
+    private StringBuilder menu_gerenciar_contatos;
+    private StringBuilder menu_criar_eventos;
+    private StringBuilder menu_gerenciar_convidados_evento;
     private GestorEventos gestorEventos;
 
+
+
+
+    // FUNÇÕES DA “PROJEÇÃO DA TELA”
     public TelaEvento() {
         gestorEventos = new GestorEventos();
-        iniciarMenu();
+        iniciarMenuAgenda();
+        iniciarMenuGerenciarContatos();
+        iniciarMenuCriarEventos();
+        iniciarMenuGerenciarConvidadosEvento();
     }
 
-    private String mostrarMenu() {
-        return JOptionPane.showInputDialog(null, menu);
+    private void iniciarMenuAgenda() {
+        menu_agenda = new StringBuilder();
+        menu_agenda.append("1 - Gerenciar Contatos").append("\n");
+        menu_agenda.append("2 - Criar Eventos").append("\n");
+        menu_agenda.append("3 - Gerenciar Convidados de evento").append("\n");
+        menu_agenda.append("0 - Sair").append("\n");
+    }
+
+    private void iniciarMenuGerenciarContatos() {
+        menu_gerenciar_contatos = new StringBuilder();
+        menu_gerenciar_contatos.append("1 - Criar Contato").append("\n");
+        menu_gerenciar_contatos.append("2 - Editar Contato").append("\n");
+        menu_gerenciar_contatos.append("3 - Excluir Contato").append("\n");
+        menu_gerenciar_contatos.append("4 - Pesquisar Contato").append("\n");
+        menu_gerenciar_contatos.append("0 - Voltar").append("\n");
+    }
+
+    private void iniciarMenuCriarEventos() {
+        menu_criar_eventos = new StringBuilder();
+        menu_criar_eventos.append("1 - Criar Evento").append("\n");
+        menu_criar_eventos.append("2 - Editar Evento").append("\n");
+        menu_criar_eventos.append("3 - Excluir Evento").append("\n");
+        menu_criar_eventos.append("4 - Pesquisar Evento").append("\n");
+        menu_criar_eventos.append("0 - Voltar").append("\n");
+    }
+
+    private void iniciarMenuGerenciarConvidadosEvento() {
+        menu_gerenciar_convidados_evento = new StringBuilder();
+        menu_gerenciar_convidados_evento.append("1 - Adicionar Convidado Individualmente").append("\n");
+        menu_gerenciar_convidados_evento.append("2 - Remover Convidado").append("\n");
+        menu_gerenciar_convidados_evento.append("3 - Mostrar lista de Convidados de um evento").append("\n");
+        menu_gerenciar_convidados_evento.append("4 - Verificar se um Contato está em uma lista de um evento").append("\n");
+        menu_gerenciar_convidados_evento.append("5 - Mostrar os eventos para os quais um contato está convidado").append("\n");
+        menu_gerenciar_convidados_evento.append("6 - Criar lista automática de Convidados").append("\n");
+        menu_gerenciar_convidados_evento.append("0 - Voltar").append("\n");
     }
 
     void executar() {
         while (true) {
-            switch (Integer.parseInt(mostrarMenu())) {
+            switch (Integer.parseInt(mostrarMenuAgenda())) {
                 case 0:
                     System.exit(0);
                 case 1:
-                    incluirEvento();
-                    incluirParticipante();
+                    executarMenuGerenciarContatos();
                     break;
                 case 2:
-                    lista();
+                    executarMenuCriarEventos();
                     break;
                 case 3:
-                    excluirEvento();
+                    executarMenuGerenciarConvidadosEvento();
                     break;
             }
         }
     }
 
-    private void iniciarMenu() {
-        menu = new StringBuilder();
-        menu.append("1 - Incluir Evento").append("\n");
-        menu.append("2 - Listar Evento").append("\n");
-        menu.append("3 - Excluir Evento").append("\n");
-        menu.append("0 - Sair").append("\n");
+    void executarMenuGerenciarContatos() {
+        while (true) {
+            switch (Integer.parseInt(mostrarMenuGerenciarContatos())) {
+                case 0:
+                    executar();
+                    break;
+                case 1:
+                    ;
+                    break;
+                case 2:
+                    ;
+                    break;
+                case 3:
+                    ;
+                    break;
+            }
+        }
     }
 
-    private void incluirEvento() {
+    void executarMenuCriarEventos() {
+        while (true) {
+            switch (Integer.parseInt(mostrarMenuCriarEventos())) {
+                case 0:
+                    executar();
+                    break;
+                case 1:
+                    criarEvento();
+                    break;
+                case 2:
+                    ;
+                    break;
+                case 3:
+                    excluirEvento();
+                    break;
+                case 4:
+                    lista();
+                    break;
+            }
+        }
+    }
+
+    void executarMenuGerenciarConvidadosEvento() {
+        while (true) {
+            switch (Integer.parseInt(mostrarMenuGerenciarConvidadosEvento())) {
+                case 0:
+                    executar();
+                    break;
+                case 1:
+                    ;
+                    break;
+                case 2:
+                    ;
+                    break;
+                case 3:
+                    ;
+                    break;
+                case 4:
+                    ;
+                    break;
+            }
+        }
+    }
+
+    private String mostrarMenuAgenda() {
+        return JOptionPane.showInputDialog(null, menu_agenda);
+    }
+
+    private String mostrarMenuGerenciarContatos() {
+        return JOptionPane.showInputDialog(null, menu_gerenciar_contatos);
+    }
+
+    private String mostrarMenuCriarEventos() {
+        return JOptionPane.showInputDialog(null, menu_criar_eventos);
+    }
+
+    private String mostrarMenuGerenciarConvidadosEvento() {
+        return JOptionPane.showInputDialog(null, menu_gerenciar_convidados_evento);
+    }
+
+
+
+
+
+    // FUNÇÕES DE “GERENCIAR CONTATOS”
+    private void incluirParticipante() {
+        Contato part = new Contato();
+        part.setNome(JOptionPane.showInputDialog("Nome"));
+        //gestorEventos.adicionaParticipante(part);
+    }
+
+
+
+
+
+    // FUNÇÕES DE “CRIAR EVENTOS”
+    private void criarEvento() {
         Eventos evento = new Eventos();
         evento.setTitulo(JOptionPane.showInputDialog("Título do Evento"));
         evento.setData(JOptionPane.showInputDialog("Data"));
@@ -51,10 +180,13 @@ public class TelaEvento {
         gestorEventos.adicionaEvento(evento);
     }
 
-    private void incluirParticipante() {
-        Contato part = new Contato();
-        part.setNome(JOptionPane.showInputDialog("Nome"));
-        //gestorEventos.adicionaParticipante(part);
+    private void excluirEvento() {
+        int indice = Integer.valueOf(JOptionPane.showInputDialog(listarEvento()));
+        gestorEventos.removeEvento(--indice);
+    }
+
+    private void lista() {
+        JOptionPane.showMessageDialog(null, listarEvento());
     }
 
     private String listarEvento() {
@@ -66,14 +198,5 @@ public class TelaEvento {
 
         }
         return listaE.toString();
-    }
-
-    private void lista() {
-        JOptionPane.showMessageDialog(null, listarEvento());
-    }
-
-    private void excluirEvento() {
-        int indice = Integer.valueOf(JOptionPane.showInputDialog(listarEvento()));
-        gestorEventos.removeEvento(--indice);
     }
 }
