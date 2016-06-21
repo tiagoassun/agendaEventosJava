a8722c4d72bb03f0662306e2395f6acaf75d1702
TiagoAssun <tiagoassun@outlook.com>
2016-06-21 15:00:01 -0300

    Modificando pacotes novamente

---
+++ src/tiagoassun/ifes/bsi/poo1/Main.java
@@ -0,0 +1,12 @@
+package tiagoassun.ifes.bsi.poo1;
+
+import tiagoassun.ifes.bsi.poo1.fronteira.TelaEvento;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //new TelaEvento().executar();
+        TelaEvento telaEvento = new TelaEvento();
+        telaEvento.executar();
+    }
+}
